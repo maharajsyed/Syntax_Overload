eb13cfc9412d9b34eaaf9f54cd04d5633d9a63bf
maharajsyed <maharajsyed@gmail.com>
2020-05-08 13:01:37 -0400

    Phase 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get "u/:username to "public#profile", as: :profile
+  get "u/:username", to: "public#profile", as: "profile"
   resources :clubs do
     resources :posts
   end
